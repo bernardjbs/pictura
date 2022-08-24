e926ce01298a89abb0197209b6950a6821e1e259
Bernard Sin Fat <bernardjbs@yahoo.com>
2022-08-24 22:59:59 +0800

    pushing to heroku

--- server/server.js
+++ server/server.js
@@ -51,5 +51,5 @@ const startApolloServer = async (typeDefs, resolvers) => {
   })
   };
   
-// Call the async function to start the server
+// Call the async function to start the server 
   startApolloServer(typeDefs, resolvers);
