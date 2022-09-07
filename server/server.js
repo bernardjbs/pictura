fa70b87522ebc01bfcff4b95ebf8667f47bec3d3
Bernard Sin Fat <bernardjbs@yahoo.com>
2022-09-07 14:10:48 +0800

    Served * to server - updating to heroku

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ app.use(cors({ origin: ["http://localhost:3000", "http://localhost:3001"] }));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
   
-  app.get('/', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
   });
 
